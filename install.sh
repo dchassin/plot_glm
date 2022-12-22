a7fbf9b76370d6109ce81702f50c4ac25c80bdf5
David P. Chassin <david.chassin@me.com>
2022-12-22 06:51:25 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+python3 -m pip install networkx
 curl -sL https://raw.githubusercontent.com/dchassin/plot_glm/main/src/plot_glm.py -o $(gridlabd --version=install)/share/gridlabd/plot_glm.py
